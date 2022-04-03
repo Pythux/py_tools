3badcdf6f64f689c00cdd1ae3d1ec05d7349331d
Pythux <pythux@nowhere.org>
2022-04-03 18:16:46 +0200

    ModuleNotFoundError on submodule
    https://stackoverflow.com/questions/55381393/modulenotfounderror-on-submodule

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     description='a little Python toolbox',
     author='Pythux',
     # author_email='',
-    packages=['tools'],  # same as name
+    packages=[
+        'tools',
+        'tools.path',
+    ],  # same as name
     # install_requires=[],  # external packages as dependencies
 )
